a82605efdc2d73097467e91ad4602a535afaa44d
r76911185 <39028339+r76911185@users.noreply.github.com>
2018-05-07 11:00:06 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -3,6 +3,6 @@
   $json_obj = json_decode($json_str);;            //轉成json格式
   
   $myfile = fopen("log.txt") or die("Unable to open file!");  //建立一個log.txt文字檔
-  fwrite($myfile, '\xEF\xBB\xBF",$json_str);                  //在字串前面加上\xEF\xBB\xBF轉成tuf8格式
+  fwrite($myfile, "\xEF\xBB\xBF",$json_str);                  //在字串前面加上\xEF\xBB\xBF轉成tuf8格式
   
 ?>
